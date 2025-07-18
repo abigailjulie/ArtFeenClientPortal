76cfc9596389a1eef4b44b939163202281148e38
Abigail <abigail.figaro@gmail.com>
2025-07-18 18:08:49 -0400

    allowed nonorigins

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -2,7 +2,9 @@ import allowedOrigins from "./allowedOrigins.js";
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1) {
+    if (!origin) {
+      callback(null, true);
+    } else if (allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
