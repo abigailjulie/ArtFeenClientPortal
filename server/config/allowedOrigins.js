9a86fa866127c4d60469892e75a0d26a8468a15a
Abigail <abigail.figaro@gmail.com>
2025-07-18 17:50:38 -0400

    added frontend url and required origin for access

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,10 +1,3 @@
-const allowedOrigins = [
-  "http://localhost:5000",
-  "http://localhost:5173",
-  "https://www.thearchway.com",
-  "https://thearchway.com",
-  "https://archway.vercel.app",
-  "https://thearchway.vercel.app",
-];
+const allowedOrigins = ["https://www.the-archway.onrender.com"];
 
 export default allowedOrigins;
