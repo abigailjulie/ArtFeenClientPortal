3edc12691552f1f3067c92f118e0eba8e2fe07b5
Abigail <abigail.figaro@gmail.com>
2025-07-18 22:21:31 -0400

    added more allowed origins

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
   "https://www.the-archway.onrender.com",
   "https://www.the-archway-backend.onrender.com",
+  "https://the-archway.onrender.com",
+  "https://the-archway-backend.onrender.com",
 ];
 
 export default allowedOrigins;
